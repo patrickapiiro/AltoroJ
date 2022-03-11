4c84348e15e53daa51687aed00214e26472db83a
Patrick Hayes <patrick@apiiro.com>
2022-03-11 16:15:54 -0500

    package

--- src/com/ibm/security/appscan/altoromutual/api/AuthController.java
+++ src/com/ibm/security/appscan/altoromutual/api/AuthController.java
@@ -1,4 +1,4 @@
-package com.example.springboot;
+package com.ibm.security.appscan.altoromutual.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
